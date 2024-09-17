250546080c3ce53f337c98c2778442c0fb58252c
luizfernando13 <43101258+luizfernando13@users.noreply.github.com>
2024-09-17 13:03:56 -0300

    Update create-goal-completion.ts

--- src/http/create-goal-completion.ts
+++ src/http/create-goal-completion.ts
@@ -1,6 +1,6 @@
 export async function createGoalCompletion(goalId: string) {
   const apiUrl = import.meta.env.VITE_API_URL
-  await fetch(`${apiUrl}/goal-completion`, {
+  await fetch(`${apiUrl}/goal-completions`, {
     method: 'POST',
     headers: {
       "Content-Type": 'application/json',
